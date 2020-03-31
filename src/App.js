b5921d8c56bc2487f19113a434ce0c551b8d11bd
Kyungsoo Han <hankevin0618@gmail.com>
2020-03-31 11:34:07 +1000

    hehe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food() {
+  return <h1>I like potato</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" > 
+      <h1>Hello
+        
+        
+        
+        </h1>
+      
     </div>
+      
   );
 }
 
