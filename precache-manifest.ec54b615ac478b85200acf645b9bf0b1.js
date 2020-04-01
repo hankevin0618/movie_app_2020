734571d3a45f2790e564fb9a8d33b79bd5edea5f
Kyungsoo Han <hankevin0618@gmail.com>
2020-04-02 00:49:09 +1000

    Updates

---
+++ precache-manifest.ec54b615ac478b85200acf645b9bf0b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b003aec09e801a5266e911ac6668ee07",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c0b5adcea4c0a265f9f0",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "cb95dd1f9fbdc9813780",
+    "url": "/movie_app_2020/static/js/2.c9fd2d69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.c9fd2d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b5adcea4c0a265f9f0",
+    "url": "/movie_app_2020/static/js/main.43e8bc29.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
