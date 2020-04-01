f914b780001d4dd3f52586de27860061bb91dde3
Kyungsoo Han <hankevin0618@gmail.com>
2020-04-02 00:41:09 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React from "react";
+// import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function Food() {
-  return <h1>I like potato</h1>
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-function App() {
-  return (
-    <div className="App" > 
-      <h1>Hello
-        
-        
-        
-        </h1>
-      
-    </div>
-      
-  );
+  getMovies = async () => {
+    // 좀 기다려야 할때는 비동기적 펑션을 쓴다
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // ES6덕분에 밑처럼 setstate를 써주면 아주 간단해짐. 스테이트 무비와 액시오스 무비를 통합해줌
+    this.setState({ movies, isLoading: false }); // movies를 가져오면 isLoading으로 넘어간다.
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state; //get things from state
+
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => {
+              // console.log(movie);
+
+              return (
+                <Movie
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
